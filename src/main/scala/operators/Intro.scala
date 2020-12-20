5530026edba4429d68529413429f2044d21b7c88
Roman Motovilov <bombey131@gmail.com>
2020-12-21 01:56:50 +0200

    added operators intro example

---
+++ src/main/scala/operators/Intro.scala
@@ -0,0 +1,21 @@
+package operators
+
+class Intro {
+  //NOTE!!! Scala does not have any operators!!!
+
+  // ++1 prefix operator
+  // 1 + 1 infix operator
+  // 1++ postfix operator
+
+  //prefix
+  val p0 = ~0 //Int bitwise negation
+  val p1 = 0.unary_~ //line 9 is the same as line 10
+
+  //infix
+  val i0 = 1 + 1 //Int addition
+  val i1 = 1.+(1) //line 14 is the same as line 15
+
+  //postfix
+  val s0 = 1 toByte //Int -> Byte conversion
+  val s1 = 1.toByte
+}
