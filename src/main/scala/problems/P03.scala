08057125a6cc90a7bb76cb7e09e2f57062caaa2b
Roman Motovilov <bombey131@gmail.com>
2020-12-28 15:00:12 +0200

    added List - find the number of elements of a list example

--- src/main/scala/problems/P03.scala
+++ src/main/scala/problems/P03.scala
@@ -1,5 +1,11 @@
 package problems
 
+// P03 (*) Find the Kth element of a list.
+//     By convention, the first element in the list is element 0.
+//
+//     Example:
+//     scala> nth(2, List(1, 1, 2, 3, 5, 8))
+//     res0: Int = 2
 object P03 {
 
   def nthBuiltin[A](n: Int, ls:List[A]): A = {
---
+++ src/main/scala/problems/P04.scala
@@ -0,0 +1,42 @@
+package problems
+
+// P04 (*) Find the number of elements of a list.
+//     Example:
+//     scala> length(List(1, 2, 3, 4))
+//     res0: Int = 4
+object P04 {
+  // Builtins.
+  def lengthBuiltin[A](ls: List[A]): Int = ls.length
+
+  // Simple recursive solution.
+  def lengthRecursive[A](ls: List[A]): Int = ls match {
+    case Nil       => 0
+    case _ :: tail => 1 + lengthRecursive(tail)
+  }
+
+  // Tail recursive solution.  Theoretically more efficient; with tail-call
+  // elimination in the compiler, this would run in constant space.
+  // Unfortunately, the JVM doesn't do tail-call elimination in the general
+  // case.  Scala *will* do it if the method is either final or is a local
+  // function.  In this case, `lengthR` is a local function, so it should
+  // be properly optimized.
+  // For more information, see
+  // http://blog.richdougherty.com/2009/04/tail-calls-tailrec-and-trampolines.html
+  def lengthTailRecursive[A](ls: List[A]): Int = {
+    def lengthR(result: Int, curList: List[A]): Int = curList match {
+      case Nil       => result
+      case _ :: tail => lengthR(result + 1, tail)
+    }
+    lengthR(0, ls)
+  }
+
+  // More pure functional solution, with folds.
+  def lengthFunctional[A](ls: List[A]): Int = ls.foldLeft(0) { (c, _) => c + 1 }
+
+  def main(args: Array[String]): Unit = {
+    println(lengthBuiltin(List(1, 2, 3, 4))) //4
+    println(lengthRecursive(List(1, 2, 3, 4))) //4
+    println(lengthTailRecursive(List(1, 2, 3, 4))) //4
+    println(lengthFunctional(List(1, 2, 3, 4))) //4
+  }
+}
