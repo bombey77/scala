3ffc0236e9f4c5d5f908c1d29c739accce0ee834
Roman Motovilov <bombey131@gmail.com>
2020-12-28 11:37:52 +0200

    added scala problem - find the last element of a list example

--- src/main/scala/problems/P01.scala
+++ src/main/scala/problems/P01.scala
@@ -1,5 +1,24 @@
 package problems
 
-class P01 {
+//Find the last element of a list
 
+//Example:
+//scala> last(List(1, 1, 2, 3, 5, 8))
+//res0: Int = 8
+object P01 {
+  // There are several ways to solve this problem.  If we use builtins, it's very
+  // easy.
+  def lastBuiltin[A](ls: List[A]): A = ls.last
+
+  // The standard functional approach is to recurse down the list until we hit
+  // the end.  Scala's pattern matching makes this easy.
+  def lastRecursive[A](ls: List[A]): A = ls match {
+    case h :: Nil  => h
+    case _ :: tail => lastRecursive(tail)
+    case _         => throw new NoSuchElementException
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(lastRecursive(List(1, 1, 2, 3, 5, 8)))
+  }
 }
