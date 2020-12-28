ce03abd1a58dc863079a13b8ff1998756cc19240
Roman Motovilov <bombey131@gmail.com>
2020-12-28 18:36:31 +0200

    added prefix operators example

---
+++ src/main/scala/prefix_operators/Demo.scala
@@ -0,0 +1,27 @@
+package prefix_operators
+
+object Demo {
+
+  def main(args: Array[String]): Unit = {
+    val x0 = +I(0)
+    val x1 = -I(0)
+    val x2 = ~I(0)
+    val x3 = !I(0)
+
+    val y0 = I(0).unary_+()
+    val y1 = I(0).unary_-()
+    val y2 = I(0).unary_~()
+    val y3 = I(0).unary_!()
+  }
+}
+
+case class I(x: Int) {
+
+  def unary_+(): I = I(2 + x)
+
+  def unary_-(): I = I(2 + x)
+
+  def unary_~(): I = I(2 + x)
+
+  def unary_!(): I = I(2 + x)
+}
