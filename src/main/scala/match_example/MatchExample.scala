ed703153eac76b89dbdf16169f615c7c0d2efc53
Roman Motovilov <bombey131@gmail.com>
2020-12-29 01:34:32 +0200

    added match example

---
+++ src/main/scala/match_example/MatchExample.scala
@@ -0,0 +1,14 @@
+package match_example
+
+object MatchExample {
+
+  def verify(x: Any): String = x match {
+    case i: Int => "This is Int " + x
+    case s: String => "This is string - ".toUpperCase() + x
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(verify(12))
+    println(verify("text"))
+  }
+}
