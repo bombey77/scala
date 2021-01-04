391041832c3933e81ebb6dba52cf0074fcc602bb
Roman Motovilov <bombey131@gmail.com>
2021-01-04 22:32:29 +0200

    added match example

--- src/main/scala/match_example/MatchExample.scala
+++ src/main/scala/match_example/MatchExample.scala
@@ -7,8 +7,19 @@ object MatchExample {
     case s: String => "This is string - ".toUpperCase() + x
   }
 
+  def verify1(obj: Any): AnyRef = obj match {
+    case Person(age, name) => name
+    case Gender(gender) => gender
+  }
+
   def main(args: Array[String]): Unit = {
     println(verify(12)) //This is Int 12
     println(verify("text")) //THIS IS STRING - text
+
+    println(verify1(Person(34, "Roman"))) // Roman
+    println(verify1(Gender("Male"))) // Male
   }
 }
+
+case class Person(age: Int, name: String)
+case class Gender(gender: String)
