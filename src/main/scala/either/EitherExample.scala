78f09d3038534b5950593a89fc7a19e5302c1f8d
Roman Motovilov <bombey131@gmail.com>
2021-01-05 17:24:05 +0200

    added either (left -> right) example

--- src/main/scala/either/EitherExample.scala
+++ src/main/scala/either/EitherExample.scala
@@ -10,5 +10,12 @@ object EitherExample {
   def main(args: Array[String]): Unit = {
     println(verify(12)) // Right(12.0)
     println(verify(-12)) // Left(x must be positive)
+
+    // Since Scala 2.12, Either has map and flatMap. These methods transform the Right case only.
+    // We say that Either is “right biased”:
+    println(Right(1).map(x => x + 1)) // Right(2)
+    println(Right(1) map { x => x + 1 }) // Right(2)
+    println(Right(1).map((x: Int) => x + 1)) // Right(2)
+    println(Left("foo").map((x: Int) => x + 1)) // Left(foo)
   }
 }
