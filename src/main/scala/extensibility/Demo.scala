c81da1d3ba4f34cc54a7c23d50911c2bd1e723b7
Roman Motovilov <bombey131@gmail.com>
2021-01-06 14:04:59 +0200

    added pattern matching (fix) example

---
+++ src/main/scala/extensibility/Demo.scala
@@ -0,0 +1,5 @@
+package extensibility
+
+object Demo {
+
+}
--- src/main/scala/match_example/PatternMatching.scala
+++ src/main/scala/match_example/PatternMatching.scala
@@ -2,7 +2,7 @@ package match_example
 
 object PatternMatching {
 
-  def verify(x: AnyRef): String = {
+  def verify(x: AnyRef): String = x match {
     case CaseClass(name) => "Case classes are work with pattern matching"
 //    case RegularClass(name) => compiler ERROR!!! => By default,
 //    pattern matching does not work with regular classes.
