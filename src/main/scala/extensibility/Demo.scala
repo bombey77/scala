ac0a530c3fc81c762ffdd8175e0400e2481a3908
Roman Motovilov <bombey131@gmail.com>
2021-01-06 15:20:53 +0200

    added extensibility example

--- src/main/scala/extensibility/Demo.scala
+++ src/main/scala/extensibility/Demo.scala
@@ -2,4 +2,10 @@ package extensibility
 
 object Demo {
 
+  class RegularClass()
+  case class CaseClass()
+  class R extends RegularClass
+  //case class T extends RegularClass => (COMPILE ERROR) a case class can not extend regular class
+  //case class V extends CaseClass => (COMPILE ERROR) a case class can not extend another case class
+  //(because it would not be possible to correctly implement their equality)
 }
