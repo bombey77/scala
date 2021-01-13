0524bfeed48a80d9012ec02844b1ad1d01661659
Roman Motovilov <bombey131@gmail.com>
2021-01-14 00:43:26 +0200

    added pattern matching example

---
+++ src/main/scala/extractors/Twice.scala
@@ -0,0 +1,5 @@
+package extractors
+
+object Twice {
+
+}
--- src/main/scala/match_example/PatternMatching.scala
+++ src/main/scala/match_example/PatternMatching.scala
@@ -7,7 +7,21 @@ object PatternMatching {
 //    case RegularClass(name) => compiler ERROR!!! => By default,
 //    pattern matching does not work with regular classes.
   }
+
+  def main(args: Array[String]): Unit = {
+    println(verify(CaseClass("Roman"))) // "Case classes are work with pattern matching"
+
+    val employee = Employee("Roman", "Moto")
+    val res = employee match {
+      case Employee("Roman", _) => "Good choice"
+      case _ => "What did you bring here?"
+    }
+    println(res) // "Good choice"
+  }
 }
 
 case class CaseClass(name: String)
 class RegularClass(name: String)
+
+// or another example
+case class Employee(firstName: String, lastName: String)
