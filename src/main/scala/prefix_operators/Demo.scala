bdf9ba15ed7452328dc89bcd4923d74b45bffe4d
Roman Motovilov <bombey131@gmail.com>
2021-01-13 11:56:58 +0200

    added prefix operator example

--- src/main/scala/prefix_operators/Demo.scala
+++ src/main/scala/prefix_operators/Demo.scala
@@ -1,7 +1,7 @@
 package prefix_operators
 
 object Demo {
-
+//  The only identifiers that can be used as prefix operators are +, -, !, and ~:
   def main(args: Array[String]): Unit = {
     val x0 = +I(0)
     val x1 = -I(0)
@@ -12,6 +12,10 @@ object Demo {
     val y1 = I(0).unary_-()
     val y2 = I(0).unary_~()
     val y3 = I(0).unary_!()
+
+    val stereo = new Stereo
+    println(+stereo)
+    println(-stereo)
   }
 }
 
@@ -24,4 +28,10 @@ case class I(x: Int) {
   def unary_~(): I = I(2 + x)
 
   def unary_!(): I = I(2 + x)
+}
+
+class Stereo {
+  def unary_+ = "on"
+
+  def unary_- = "off"
 }
