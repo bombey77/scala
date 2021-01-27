b739cb4d1213dd62537f000e31b54070246d40fd
Roman Motovilov <bombey131@gmail.com>
2021-01-27 18:24:01 +0200

    added rotate a list N place to the left examples

---
+++ src/main/scala/problems/P19.scala
@@ -0,0 +1,29 @@
+package problems
+
+// P19 (**) Rotate a list N places to the left.
+//     Examples:
+//     scala> rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+//     res0: List[Symbol] = List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
+//
+//     scala> rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+//     res1: List[Symbol] = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+object P19 {
+
+  def main(args: Array[String]): Unit = {
+    println(rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+    println(rotate1(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+    println(rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+    println(rotate1(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+  }
+
+  def rotate[A](n: Int, ls: List[A]): List[A] = {
+    if (n > 0) ls.drop(n) ::: ls.take(n)
+    else ls.takeRight(n.abs) ::: ls.dropRight(n.abs)
+  }
+
+  def rotate1[A](n: Int, ls: List[A]): List[A] = {
+    val nBounded = if (ls.isEmpty) 0 else n % ls.length
+    if (nBounded < 0) rotate(nBounded + ls.length, ls)
+    else (ls drop nBounded) ::: (ls take nBounded)
+  }
+}
