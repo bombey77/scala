883c0568e2bb373a0fd4554524d9382ce3addfa8
Roman Motovilov <bombey131@gmail.com>
2021-01-28 15:23:58 +0200

    added generate a random permutation of the elements of a list example

---
+++ src/main/scala/problems/P25.scala
@@ -0,0 +1,17 @@
+package problems
+
+// P25 (*) Generate a random permutation of the elements of a list.
+//     Hint: Use the solution of problem P23.
+//
+//     Example:
+//     scala> randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+//     res0: List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)
+object P25 {
+
+  def main(args: Array[String]): Unit = {
+    println(randomPermute(List('a, 'b, 'c, 'd, 'e, 'f)))
+  }
+
+  import P23.randomSelect
+  def randomPermute[A](ls: List[A]): List[A] = randomSelect(ls.size, ls)
+}
