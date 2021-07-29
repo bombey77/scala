d5580b28cb9a8457fc74c83adcd62d1aaf8a6b57
Motovilov Roman <bombey131@gmail.com>
2021-07-29 23:01:16 +0300

    add type definition example

--- src/main/scala/type_definition/Demo.scala
+++ src/main/scala/type_definition/Demo.scala
@@ -2,8 +2,9 @@ package type_definition
 
 trait T
 class C
+object O
 
-//объявление объекта создает создает singleton
+//объявление объекта создает singleton
 object OT extends T //можно наследоваться от trait
 object CT extends C //можно наследоваться от класса
 
@@ -13,6 +14,9 @@ class Demo {
 
   tFunc(new T {}) //передаю trait и объявляю его
   tFunc(OT)
+
+//  def oFunc(o: O): O = ??? -> ошибка компиляции, ниже пример компилируется
+  def oFunc(o: O.type): O.type = ???
 
   def cFunc(c: C): Unit = ???
 
@@ -24,5 +28,7 @@ class Demo {
     case _: C => "C"
 //    case _: OT => "OT" -> ошибка компиляции
     case _: OT.type => "OT"
+    case OT => "OT"
+    case O => "O"
   }
 }
